2bb66e0149407304f87109f9d4ee8319e4592de4
heolyun <hl620@naver.com>
2026-04-14 19:31:57 +0900

    Switch deployment setup to Netlify

--- scripts/copy-404.mjs
+++
@@ -1,6 +0,0 @@
-import { copyFile } from 'node:fs/promises'
-import { resolve } from 'node:path'
-
-const distDir = resolve(process.cwd(), 'dist')
-
-await copyFile(resolve(distDir, 'index.html'), resolve(distDir, '404.html'))
